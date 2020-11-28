a3d5a442ef0af62eb500660ba93a38d5f82df307
DONGHOON IM <dongttang@gmail.com>
2020-11-28 19:51:31 +0900

    Add stack overflow scraper #4

--- scraper/stack_overflow.py
+++ scraper/stack_overflow.py
@@ -1,37 +1,28 @@
-from config import *
+from bs4 import BeautifulSoup
+import requests
+from scraper import config
 
 BASE_URL = "https://stackoverflow.com/"
 result_dict = {}
-result_dict["stack_overflow"] = []
 
 
-def scrap_so(keyword=""):
+def scrap(keyword=""):
     query = f"jobs?r=true&q={keyword}"
-    target_url = BASE_URL + query
-    result_dict["stack_overflow"] = {
-        "keyword": keyword, "url": target_url, "result": []}
-
-    response = requests.get(target_url, headers=HEADER)
-
+    target_url = f"{BASE_URL}{query}"
+    result_dict = {
+        "site": "stack_overflow", "keyword": keyword, "url": target_url, "result": []}
+    response = requests.get(target_url, headers=config.HEADER)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
         raw_job_list = soup.find(
             "div", {"class": "listResults"}).find_all("div", {"class": "js-result"})
         for job in raw_job_list:
-            job_card = job_card_template.copy()
+            job_card = config.job_card_template.copy()
             job_card["title"] = job.find(
                 "a", {"class": "s-link"})["title"]
             job_card["url"] = BASE_URL + \
                 job.find("a", {"class": "s-link"})["href"]
             job_card["company_name"] = job.find(
                 "h3", {"class": "fc-black-700"}).find("span").text
-            result_dict["stack_overflow"]["result"].append(job_card)
-
-        f = open("./DUMMY_codes/sample_so.json", "w")
-        f.write(str(result_dict))
-        f.close()
-
-    return raw_job_list
-
-
-print(scrap_so("python"))
+            result_dict["result"].append(job_card)
+    return result_dict
