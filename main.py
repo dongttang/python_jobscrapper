dfea75a9afd915dacad2040a80d8d54bf6ffbdcd
DONGHOON IM <dongttang@gmail.com>
2020-11-28 19:55:42 +0900

    Add main.py #9

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template, request
+from scraper import config, remote_ok, stack_overflow, we_work_remotely
 
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
-}
-
+SCRAPER_LIST = config.REGISTERED_SCRAPER
+headers = config.HEADER
 app = Flask("DayEleven")
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", scraper_list=SCRAPER_LIST)
+
+
+@app.route("/result")
+def result():
+    scraped_site_list = []
+    keyword = request.args.get("keyword")
+    print(keyword)
+    for scraper_name in SCRAPER_LIST:
+        if request.args.get(scraper_name):
+            function_name = f"{scraper_name}.scrap"
+            print(function_name + f"({keyword})")
+            scraped_site_list.append(eval(function_name + f"(\"{keyword}\")"))
+
+    return render_template("searchResults.html", scraped_site_list=scraped_site_list)
 
 
 # app.run(host="0.0.0.0")
