f2edd4532f7940850ab63187eae9d7cfe29e2885
DONGHOON IM <dongttang@gmail.com>
2020-11-28 19:50:32 +0900

    Add config.py for common features of each scraper

--- scraper/config.py
+++ scraper/config.py
@@ -1,6 +1,3 @@
-from bs4 import BeautifulSoup
-import requests
-
 HEADER = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
 }
